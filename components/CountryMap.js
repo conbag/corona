6d95ac8ee9917a2abb80a5c965fd984347dddcc4
conbag <cogrady91@gmail.com>
2020-03-15 17:06:25 +0000

    adding color scale based on confirmed vs population

--- components/CountryMap.js
+++ components/CountryMap.js
@@ -4,10 +4,16 @@ import {
   Geographies,
   Geography,
 } from "react-simple-maps"
+import { scaleLinear } from "d3-scale";
 import { useState, useEffect } from "react"
 
 // url to a valid topojson file
 const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json"
+
+const CONFIRMED_POPULATION_HIGH_DOMAIN = 0.0003
+const colorScale = scaleLinear()
+  .domain([0.0, CONFIRMED_POPULATION_HIGH_DOMAIN])
+  .range(["#ffedea", "#ff5233"]);
 
 export default function CountryMap({ setTooltipContent }) {
     const [countryStats, setCountryStats] = useState();
@@ -16,18 +22,16 @@ export default function CountryMap({ setTooltipContent }) {
         fetch(`https://covid19.mathdro.id/api/confirmed/`)
         .then(data => data.json())
         .then(res => {                
-            const countryData = reduceCountryData(res)
-
-            console.log(countryData)
-            setCountryStats(countryData)            
+            const countryData = reduceCountryData(res);
+            console.log(countryData);
+            setCountryStats(countryData);       
         })        
     }, [])
     // empty array as second argument ensures useEffect function only runs once on initial render: https://css-tricks.com/run-useeffect-only-once/ 
 
     return (
-        <div>
-            <ComposableMap 
-                data-tip="" 
+        <div data-tip="">
+            {countryStats ? <ComposableMap                
                 projectionConfig={{
                     scale: 155,
                     rotation: [-11, 0, 0],
@@ -37,40 +41,47 @@ export default function CountryMap({ setTooltipContent }) {
                 style={{ width: "100%", height: "auto" }}
             >
                 <Geographies geography={geoUrl}>
-                {({ geographies }) => geographies.map(geo =>
-                    <Geography 
-                        key={geo.rsmKey} 
-                        geography={geo} 
-                        onMouseEnter={() => {
-                            const iso = geo.properties.ISO_A2
-                            if(countryStats[iso]) {
-                                const {confirmed, recovered, deaths} = countryStats[iso]
-                                setTooltipContent(`${geo.properties.NAME}<br><br>Confirmed: ${confirmed.toLocaleString()}<br>Recovered: ${recovered.toLocaleString()}<br>Deaths: ${deaths.toLocaleString()}`);
-                            } else {
-                                setTooltipContent(`${geo.properties.NAME}<br><br>No data available`)
-                            }
-                        }}
-                        onMouseLeave={() => {
-                            setTooltipContent(``);
-                        }}
-                        style={{
-                            default: {
-                                fill: "#D6D6DA",
-                                outline: "none"
-                            },
-                            hover: {
-                                fill: "#F53",
-                                outline: "none"
-                            },
-                            pressed: {
-                                fill: "#E42",
-                                outline: "none"
-                            }
-                        }} 
-                    />
-                )}
+                {({ geographies }) => geographies.map(geo => {
+                    //console.log(geo)
+                    const {ISO_A2, POP_EST, NAME} = geo.properties
+                    const {confirmed, recovered, deaths} = countryStats[ISO_A2] || {}                    
+
+                    //console.log(confirmed / POP_EST)
+
+                    return (
+                        <Geography 
+                            key={geo.rsmKey} 
+                            geography={geo} 
+                            fill={countryStats[ISO_A2] ? 
+                                (colorScale(confirmed / POP_EST)) : 
+                                "#F5F4F6"}
+                            onMouseEnter={() => {                                                                                            
+                                countryStats[ISO_A2] ?
+                                    setTooltipContent(`${NAME}<br><br>Confirmed: ${confirmed.toLocaleString()}<br>Recovered: ${recovered.toLocaleString()}<br>Deaths: ${deaths.toLocaleString()}`) :
+                                    setTooltipContent(`${NAME}<br><br>No data available`)
+                            }}
+                            onMouseLeave={() => {
+                                setTooltipContent(``);
+                            }}
+                            style={{
+                                default: {
+                                    //fill: "#D6D6DA",
+                                    outline: "none"
+                                },
+                                hover: {
+                                    fill: "#F53",
+                                    outline: "none"
+                                },
+                                pressed: {
+                                    fill: "#E42",
+                                    outline: "none"
+                                }
+                            }} 
+                        />
+                    )
+                })}
                 </Geographies>
-            </ComposableMap>      
+            </ComposableMap> : <p></p>}     
         </div>
     )
 }
